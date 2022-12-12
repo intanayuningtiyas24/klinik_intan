c4cff06260406d18c5cd2cd86c99c3c4bdbf5fcd
intanayuningtiyas24 <intanayuningtiyas6@gmail.com>
2022-12-12 11:52:41 +0700

    upload github

---
+++ application/views/v_dashboard.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+    <div class="jumbotron">
+        <h1 class="display-4">KLINIK INTAN</h1>
+        <p class="lead">WELCOME.</p>
+        <hr class="my-4">
+    </div>
+    <br><br>
+    <!-- <img src="<?= base_url('assets/orang.gif') ?>" alt=""> -->
+</div>
